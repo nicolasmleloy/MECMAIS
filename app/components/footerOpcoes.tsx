fcc3c8828eae9cd8bc67d491e08d7ceed2741d18
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2025-07-11 17:05:56 -0400

    Teste new branch 3

---
+++ app/components/footerOpcoes.tsx
@@ -0,0 +1,58 @@
+import { TouchableOpacity, View, Image } from "react-native";
+import React from 'react';
+
+type FooterProps = {
+    pagina: "dashboard" | "cardapio" | "notificacoes";
+};
+
+export default function FooterOpcoes({pagina}: FooterProps){
+    switch (pagina) {
+        case "dashboard":
+            return(
+                <View className="flex-row justify-around w-full fixed bottom-0 left-0 z-99 p-4">
+                    <TouchableOpacity className="flex justify-center items-center bg-gray-800 rounded-xl p-2">
+                        <Image source={require('../../assets/images/icone-ok.png')} width={20}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                        <Image source={require('../../assets/images/icone-cardapio.png')} width={20}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                        <Image source={require('../../assets/images/icone-sino.png')} width={20}/>
+                    </TouchableOpacity>
+                </View>
+            )
+            break;
+            case "cardapio":
+                return(
+                    <View className="flex-row justify-around w-full fixed bottom-0 left-0 z-99 p-4">
+                        <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                            <Image source={require('../../assets/images/icone-ok.png')} width={20}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity className="flex justify-center items-center bg-gray-800 rounded-xl p-2">
+                            <Image source={require('../../assets/images/icone-cardapio.png')} width={20}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                            <Image source={require('../../assets/images/icone-sino.png')} width={20}/>
+                        </TouchableOpacity>
+                    </View>
+                )
+                break;
+                case "notificacoes": 
+                    return(
+                        <View className="flex-row justify-around w-full fixed bottom-0 left-0 z-99 p-4">
+                            <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                                <Image source={require('../../assets/images/icone-ok.png')} width={20}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="flex justify-center items-center bg-[#0E57C6] rounded-xl p-2">
+                                <Image source={require('../../assets/images/icone-cardapio.png')} width={20}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="flex justify-center items-center bg-gray-800 rounded-xl p-2">
+                                <Image source={require('../../assets/images/icone-sino.png')} width={20}/>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                break;
+        default:
+            break;
+    }
+}
